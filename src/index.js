346151cfe10a859bcdf2e1c6cf04fda99077a918
Daniel Cousens <github@dcousens.com>
2015-06-10 16:22:22 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+module.exports = function () {
+  var listeners = []
+
+  return {
+    componentWillUnmount () {
+      listeners.forEach(function (listener) {
+        var { emitter, eventName, callback } = listener
+
+        emitter.removeListener(eventName, callback)
+      })
+    },
+
+    on (emitter, eventName, callback) {
+      listeners.push({
+        emitter: emitter,
+        eventName: eventName,
+        callback: callback
+      })
+
+      emitter.on(eventName, callback)
+    },
+
+    removeListener (emitter, eventName, callback) {
+      listeners = listeners.filter((listener) => {
+        return listener.emitter === emitter &&
+               listener.eventName === eventName &&
+               listener.callback === callback
+      })
+    }
+  }
+}
